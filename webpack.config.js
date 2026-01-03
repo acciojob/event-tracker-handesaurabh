8b07e41d94f50474c0caf38a672c9b579cf52bfe
handesaurabh <handesaurabh2004@gmail.com>
2026-01-03 22:55:59 +0530

    config

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,56 @@
 const path = require("path");
-const HtmlWebpackPlugin= require('html-webpack-plugin');
+const webpack = require('webpack'); // Add this import
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    entry: './src/index.js',
+module.exports = (env, argv) => { // Accept env/argv from CLI
+    const isTest = argv.mode === 'test'; // Detect test mode
 
-    output: {
-        path: path.join(__dirname,"/dist"),
-        filename: "index_bundle.js",
-    },
-    module:{
-        rules: [
-            {
-                test: /\.js$|\.jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
+    return {
+        mode: isTest ? 'test' : 'development', // Set mode explicitly
+        entry: './src/index.js',
+        output: {
+            path: path.join(__dirname, "/dist"),
+            filename: "index_bundle.js",
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$|\.jsx$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        { loader: 'style-loader' },
+                        { loader: 'css-loader' }
+                    ]
                 }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-                ]
-            },
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./src/index.html"
+            }),
+            // Define NODE_ENV globally for React
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify(isTest ? 'test' : 'development')
+            })
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
-    ],
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    // Add this to fix Node.js compatibility issues
-    node: {
-        crypto: 'empty',
-        stream: 'empty'
-    }
+        resolve: {
+            extensions: ['.js', '.jsx']
+        },
+        devServer: { // Add devServer for npm start
+            port: 3000,
+            open: true,
+            hot: true,
+            static: path.join(__dirname, 'dist')
+        },
+        node: {
+            crypto: 'empty',
+            stream: 'empty'
+        }
+    };
 };
